456646ace874b8753cd794b1ecdf843d201e66ff
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-12-31 19:34:36 -0600

    fix: reduce duration in ScrollDown component for improved performance

--- src/App.jsx
+++ src/App.jsx
@@ -87,7 +87,7 @@ const App = () => {
         <LugarFecha />
         <CountdownTimer />
       </VerticalSlider>
-      <ScrollDown dur={25000} />
+      <ScrollDown dur={20000} />
     </>
   );
 };
